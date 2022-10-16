799494bf17f9ae68d1e091962702692240a7d60d
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-10-16 22:52:48 +0100

    Create action.py

---
+++ src/action.py
@@ -0,0 +1,5 @@
+# A simple "Hello, World!" action.
+#
+# Replace the entire content of this file with your own content.
+# After replacing the code, that code will be executed when the Actions workflow
+# starts this action.
